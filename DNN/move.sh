3716fb5dea067d903c968d373e80597ce3a965b7
Jokilos <huhun@onet.pl>
2024-12-17 17:38:36 +0100

    dnn/swp

--- DNN/move.sh
+++
@@ -1,8 +0,0 @@
-for dir in lab[1-9]; do
-    if [ -d "$dir" ]; then
-        new_dir=$(printf "lab%02d" "${dir#lab}")
-        echo $new_dir
-        # Rename the directory
-        mv "$dir" "$new_dir"
-    fi
-done
